e1391678a9a50e94525996fa9ddab5f9c985d16f
Khánh Phạm Phú <phamphukhanh.sgu.edu@gmail.com>
2024-03-20 21:59:04 +0700

    change default url to home page

--- home/views.py
+++ home/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+def index(request):
+   response = HttpResponse()
+   response.writelines('<h1>Xin chào</h1>')
+   response.write('Đây là app home')
+   return response
