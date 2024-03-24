43aafa9033416c3819089d449127f4fb14a31245
Khánh Phạm Phú <phamphukhanh.sgu.edu@gmail.com>
2024-03-24 13:48:36 +0700

    create note model
    and switch SQLite to MySQL

--- note_taking_application/settings.py
+++ note_taking_application/settings.py
@@ -76,8 +76,8 @@ WSGI_APPLICATION = 'note_taking_application.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'note_management',
     }
 }
 
