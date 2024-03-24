657bfd342061b365c07d453922ac26695f0d8a08
Khánh Phạm Phú <phamphukhanh.sgu.edu@gmail.com>
2024-03-25 00:39:19 +0700

    add filter to query

--- home/views.py
+++ home/views.py
@@ -9,7 +9,11 @@ from django.http import HttpResponse
 
 
 def note_list(request):
-    Data = {'Notes': Note.objects.all().order_by('-date_modified')}
+    # Query all notes and order by date_modified
+    # Data = {'Notes': Note.objects.all().order_by('-date_modified')}
+
+    # Query all notes of userthihong@example.com and order by date_modified
+    Data = {'Notes': Note.objects.filter(username="thihong@example.com").order_by('-date_modified')}
     return render(request, 'pages/home.html', Data) 
 
 
