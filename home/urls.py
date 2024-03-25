b0db402f225b0e5eaca5335de166970d59662dc9
Khánh Phạm Phú <phamphukhanh.sgu.edu@gmail.com>
2024-03-25 20:07:29 +0700

    add search note feature
    query on fixed data, will modify later

--- home/urls.py
+++ home/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-   path('', views.note_list)
+    path('', views.note_list),
+    path('search/', views.find_note, name='find_note'),
 ]
--- home/views.py
+++ home/views.py
@@ -1,10 +1,8 @@
 from django.shortcuts import render
 from .models import Note
-
-
-# Create your views here.
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.db.models import Q
 # Create your views here.
 
 
@@ -13,8 +11,23 @@ def note_list(request):
     # Data = {'Notes': Note.objects.all().order_by('-date_modified')}
 
     # Query all notes of userthihong@example.com and order by date_modified
-    Data = {'Notes': Note.objects.filter(username="thihong@example.com").order_by('-date_modified')}
-    return render(request, 'pages/home.html', Data) 
+    Data = {'Notes': Note.objects.filter(
+        username="thihong@example.com").order_by('-date_modified')}
+    return render(request, 'pages/home.html', Data)
+
+
+def find_note(request):
+    query = request.GET.get('query', '').strip()
+    if query == '':
+        return render(request, 'pages/home.html')
+    else:
+        Data = {
+            'Notes': Note.objects.filter(
+                Q(title__icontains=query) | Q(content__icontains=query),
+                username="thihong@example.com"
+            ).order_by('-date_modified')
+        }
+        return render(request, 'pages/home.html', Data)
 
 
 def index(request):
